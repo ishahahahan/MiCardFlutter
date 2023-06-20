b14992a2a691553b68a8fbf91435391e40de0b85
ForsythIshan <ishand1005@gmail.com>
2023-06-20 21:13:00 +0530

    20/06 commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Flutter\mi_card_flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Flutter\Projects\3. mi_card_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,42 +43,64 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade100,
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.teal.shade900,
-                    ),
-                    title: Text(
-                      '+91 96358 72209',
-                      style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20,
-                          color: Colors.teal.shade900),
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Padding(
+              GestureDetector(
+                onTap: () async {
+                  Uri phoneno = Uri.parse('tel:+919635872209');
+                  if (await launchUrl(phoneno)) {
+                    launchUrl(phoneno);
+                  } else {
+                    AlertDialog(
+                      content: Text('Can\'t Call'),
+                    );
+                  }
+                },
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
                     padding: const EdgeInsets.all(5.0),
                     child: ListTile(
                       leading: Icon(
-                        Icons.mail,
+                        Icons.phone,
                         color: Colors.teal.shade900,
                       ),
                       title: Text(
-                        'ishand1005@gmail.com',
+                        '+91 96358 72209',
                         style: TextStyle(
                             fontFamily: 'Source Sans Pro',
                             fontSize: 20,
                             color: Colors.teal.shade900),
                       ),
-                    )),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  final mailToUri = Uri(
+                      scheme: 'mailto',
+                      path: 'ishand1005@gmail.com',
+                      queryParameters: {'mailto': 'ishand1005@gmail.com'});
+                  print(mailToUri);
+                  launchUrl(mailToUri);
+                },
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.mail,
+                          color: Colors.teal.shade900,
+                        ),
+                        title: Text(
+                          'ishand1005@gmail.com',
+                          style: TextStyle(
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20,
+                              color: Colors.teal.shade900),
+                        ),
+                      )),
+                ),
               ),
             ],
           ),
